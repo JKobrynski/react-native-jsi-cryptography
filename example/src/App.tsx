9bd507a19bd95bfc0416f252ac5a517e41e61a84
Julian Kobrynski <julian.kobrynski@netguru.com>
2022-01-28 21:18:57 +0100

    App example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,23 +1,83 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 
-import { StyleSheet, View, Text } from 'react-native';
-// import { multiply } from 'react-native-jsi-cryptography';
+const functions = [
+  'sha1',
+  'md5',
+  'sha224',
+  'sha256',
+  'sha384',
+  'sha512',
+] as const;
+
+type Method = typeof functions[number];
+
+const callMethod = (method: Method, value: string): string => {
+  switch (method) {
+    case 'sha1':
+      return sha1(value);
+    case 'md5':
+      return md5(value);
+    case 'sha224':
+      return sha224(value);
+    case 'sha256':
+      return sha256(value);
+    case 'sha384':
+      return sha384(value);
+    case 'sha512':
+      return sha512(value);
+  }
+};
 
 export default function App() {
-  // const [result, setResult] = React.useState<number | undefined>();
+  const [activeMethod, setActiveMethod] = useState<Method>('sha1');
+  const [value, setValue] = useState('netguru');
 
-  // React.useEffect(() => {
-  //   multiply(3, 7).then(setResult);
-  // }, []);
+  const onPress = (method: Method) => setActiveMethod(method);
 
   return (
     <View style={styles.container}>
-      <Text>sha1: {sha1('grape')}</Text>
-      <Text>md5: {md5('grape')}</Text>
-      <Text>sha224: {sha224('grape')}</Text>
-      <Text>sha256: {sha256('grape')}</Text>
-      <Text>sha384: {sha384('grape')}</Text>
-      <Text>sha512: {sha512('grape')}</Text>
+      <View style={styles.flexed}>
+        <Text style={styles.activeMethod}>{activeMethod}</Text>
+        <Text>{callMethod(activeMethod, value)}</Text>
+      </View>
+      <View style={styles.flexed}>
+        <TextInput
+          value={value}
+          onChangeText={setValue}
+          style={styles.input}
+          autoCapitalize="none"
+        />
+      </View>
+      <View style={styles.flexed}>
+        <View style={styles.row}>
+          {functions.map((item) => (
+            <TouchableOpacity
+              onPress={() => onPress(item)}
+              style={
+                activeMethod === item
+                  ? styles.activeVariantWrapper
+                  : styles.variantWrapper
+              }
+            >
+              <Text
+                key={item}
+                style={
+                  activeMethod === item ? styles.activeVariant : styles.variant
+                }
+              >
+                {item}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
     </View>
   );
 }
@@ -27,10 +87,56 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 24,
+    paddingVertical: 100,
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+    flexWrap: 'wrap',
+  },
+  flexed: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  activeMethod: {
+    fontSize: 32,
+    fontWeight: '600',
+    marginBottom: 40,
+  },
+  variantWrapper: {
+    width: '28%',
+    alignItems: 'center',
+    marginBottom: 32,
+    backgroundColor: '#eee',
+    paddingVertical: 12,
+    borderRadius: 16,
+  },
+  activeVariantWrapper: {
+    width: '28%',
+    alignItems: 'center',
+    marginBottom: 32,
+    backgroundColor: '#000',
+    paddingVertical: 12,
+    borderRadius: 16,
+  },
+  variant: {
+    fontSize: 16,
+    color: '#000',
+    fontWeight: '500',
+  },
+  activeVariant: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: '500',
+  },
+  input: {
+    width: '100%',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    fontSize: 20,
   },
 });
